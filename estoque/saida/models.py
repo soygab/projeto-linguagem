7c7681ba9967fd728e53a4894cc0ae196963bd41
soygab <gabrielrodrigues13012007@gmail.com>
2026-06-15 09:56:42 -0300

    feat: criação da tabela "Saídas" em models

--- estoque/saida/models.py
+++ estoque/saida/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from produto.models import Produtos
 
-# Create your models here.
+class Saidas(models.Model):
+    produto = models.ForeignKey(
+        Produtos,
+        on_delete=models.CASCADE,
+        verbose_name='Produto'
+    )
+
+    preco = models.FloatField('Preço', default=0)
+    quantidade = models.IntegerField('Quantidade', default=0)
+
+    criado = models.DateTimeField(
+        'Criado em',
+        auto_now_add=True
+    )
+
+    modificado = models.DateTimeField(
+        'Modificado em',
+        auto_now=True
+    )
